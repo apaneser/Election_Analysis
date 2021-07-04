156bc016303f64ce093c6b87d89f932449b63a1e
apaneser <56094764+apaneser@users.noreply.github.com>
2021-07-03 20:43:16 -0700

    update sec 4

--- PyPoll.py
+++ PyPoll.py
@@ -7,6 +7,13 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 #open file 
 with open(file_to_load) as election_data:
 
+    # read the file object with the reader function
+    file_reader = csv.reader(election_data)
+
+    # print each row in the csv file
+    headers = next(file_reader)
+    print(headers) 
+
     # The data we want to receive
     # 1. The total number of votes cast
     # 2. A complete list of candidates who received votes
@@ -20,10 +27,7 @@ with open(file_to_load) as election_data:
 #save path of file to open in variable
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 #open file in write mode
-outfile = open(file_to_save, "w")
+with open(file_to_save, "w") as txt_file:
 
-# write data to file
-outfile.write("Hello World")
-
-# close file
-outfile.close()
+    # write data to file
+    txt_file.write("Arapahoe\nDenver\nJefferson")
