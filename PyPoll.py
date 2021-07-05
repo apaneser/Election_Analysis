1fc42a0a9fefe5c70cc9b5d984908c62ac600374
apaneser <56094764+apaneser@users.noreply.github.com>
2021-07-04 20:57:01 -0700

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -22,6 +22,11 @@ candidate_options = []
 # dictionary of candidates and number of votes
 candidate_votes = {}
 
+# winning candidate and winning count tracker
+winning_candidate = ''
+winning_count = 0
+winning_percentage = 0
+
 #open file 
 with open(file_to_load) as election_data:
 
@@ -46,14 +51,41 @@ with open(file_to_load) as election_data:
         
         candidate_votes[candidate_name] += 1
 
+    # get percentage of votes for each candidate through looping
+    for candidate_name in candidate_votes:
+        # get number of votes for the candidate
+        votes = candidate_votes[candidate_name]
+        # calculate percentage of total vote
+        vote_percentage = float(votes) / float(total_votes) * 100
+        # print result for each candidate
+        print(f'{candidate_name}: {vote_percentage:.1f}% ({votes:,})')
+
+    
+        # determine winning vote count and candidate
+        # determine if the votes is greater than the winning count
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # if true, set winning count = votes and winning_percent = vote percentage
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n"
+    )
+    print(winning_candidate_summary)
+
     # print the total votes
-    print(total_votes)
+    #print(total_votes)
 
     # print candidates
-    print(candidate_options)
+    #print(candidate_options)
 
     # print candidate vote dictionary
-    print(candidate_votes)
+    #print(candidate_votes)
 
 
 #open file in write mode
